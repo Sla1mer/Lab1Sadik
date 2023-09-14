a67bde52a6c91a6058ead17090ffbc11ee1f8df9
sadiknazar <sadiknazar@945B>
2023-09-14 17:57:59 +0200

    Добавьте файлы проекта.

---
+++ Lab1Sadik/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введите b: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Введите x: ");
+double x = double.Parse(Console.ReadLine());
+
+double R = ((a) / (x - a)) + ((Math.Pow(b, x) + Math.Pow(Math.Cos(x), 3)) / (Math.Pow(Math.Log(a), 3) + 4.5));
+Console.WriteLine($"R = {R:F2}");
